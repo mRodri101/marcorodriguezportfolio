7922cb0ca4fa5438aaf49ca76b4371718b583c20
mRodri101 <71103397+mRodri101@users.noreply.github.com>
2020-12-11 12:22:11 -0600

    sercret sauce

--- main.js
+++ main.js
@@ -10,13 +10,29 @@ menuBtn.addEventListener('click', () => {
 });
 */
 const parentmenuBtn = document.querySelector(".menu-btn");
+
 const parentmenuItems = document.querySelector(".menu-items");
 const parentmenuItem = document.querySelectorAll(".menu-item");
 
 // main toggle
-parentmenuBtn.addEventListener("click", () => {
+// parentmenuBtn.addEventListener("click", () => {
+//   toggle();
+  
+  
+// });
+
+let menuShowing = false;
+$('.menu-btn').click(function(){
   toggle();
-});
+
+  if(!menuShowing) {
+    $('.sidenav').addClass("slidein").removeClass("slideout");
+    menuShowing = true;
+  } else {
+    $('.sidenav').addClass("slideout").removeClass("slidein");
+    menuShowing = false;
+  }
+})
 
 // toggle on item click if open
 parentmenuItem.forEach((item) => {
