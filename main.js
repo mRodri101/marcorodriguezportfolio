0d9d896c8eb7cae83befba8669f1441123feb441
mRodri101 <71103397+mRodri101@users.noreply.github.com>
2020-12-16 18:39:47 -0600

    lots

--- main.js
+++ main.js
@@ -1,25 +1,7 @@
-/*
-const menuBtn = document.querySelector('.menu-btn');
-const menuItems = document.querySelector(".menu-items")
-let menuOpen = false;
-
-// main toggle
-menuBtn.addEventListener('click', () => {
-    menuBtn.classList.toggle('open');
-    menuItems.classList.remove('open');
-});
-*/
 const parentmenuBtn = document.querySelector(".menu-btn");
 
 const parentmenuItems = document.querySelector(".menu-items");
 const parentmenuItem = document.querySelectorAll(".menu-item");
-
-// main toggle
-// parentmenuBtn.addEventListener("click", () => {
-//   toggle();
-  
-  
-// });
 
 let menuShowing = false;
 $('.menu-btn').click(function(){
